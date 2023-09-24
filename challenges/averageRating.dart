469333b6530bb814c7a3abe3a3e58a8e99d6e80d
theghostmac <theghostmac@gmail.com>
2023-09-25 00:04:49 +0100

    feature: finished 2 challenges

---
+++ challenges/averageRating.dart
@@ -0,0 +1,13 @@
+void averageRating() {
+    double rating1 = 10.5;
+    double rating2 = 11.5;
+    double rating3 = 12.5;
+
+    var averageRating;
+    averageRating = (rating1 + rating2 + rating3) / 3;
+    print(averageRating);
+}
+
+void main() {
+    averageRating();
+}
---
+++ challenges/computeAnswer.dart
@@ -0,0 +1,9 @@
+void stuff() {
+    const x = 46;
+    const y = 10;
+
+
+    const answer1 = (x * 100) + y; // 4610;
+    const answer2 = (x * 100) + (y * 100); // 4600 + 1000 = 5600
+    const answer3 = (x * 100) + (y / 100); // 4600 + 0.1 = 4600.1
+}
