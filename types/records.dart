9674f500645508989ba8e3470acd422aa45b8792
theghostmac <theghostmac@gmail.com>
2023-09-24 23:23:43 +0100

    task: moved homer

--- types/records.dart
+++ types/records.dart
@@ -1,7 +1,6 @@
-// Records are heterogenous, typed, fixed/immutable.
-
-void create_record() [
+void createRecord() {
     var record = ('first', a: 2, b: true, 'last', 35);
+    print(record);
 }
 
 (int, int) swap((int, int) record) {
@@ -9,9 +8,33 @@ void create_record() [
     return (b, a);
 }
 
-void record_annotation() {
+void recordAnnotation() {
     ({int a, bool b}) record;
     record = (a: 123, b: true);
     print(record);
     print(record.a);
+}
+
+(String, int) userInfo(Map<String, dynamic> json) {
+    return (json['name'] as String, json['age'] as int);
+}
+
+void main() {
+    createRecord();
+
+    var record = (3, 5);
+    var swappedRecord = swap(record);
+    print('Swapped Record: $swappedRecord');
+
+    recordAnnotation();
+
+    final json = <String, dynamic> {
+        'name': 'Dash',
+        'age': 10,
+        'color': 'blue',
+    };
+
+    var (name, age) = userInfo(json);
+    print("My name is: $name\n");
+    print("My age is: $age\n");
 }
