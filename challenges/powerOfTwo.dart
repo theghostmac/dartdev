4333e3f5083ebbe9a993d0c08d8b220d798c3187
theghostmac <theghostmac@gmail.com>
2023-10-06 12:25:26 +0100

    chore: solved power of two

--- challenges/powerOfTwo.dart
+++ challenges/powerOfTwo.dart
@@ -1,19 +1,29 @@
-void powerOfTwo() {
+// int nextPowerOfTwo(int n) {
+//   int result = 1;
+//   while (result < n) {
+//     result *= 2;
+//   }
+//   return result;
+// }
+//
+// void main() {
+//   int number = 17; // Change this to your desired input number
+//   int nextPower = nextPowerOfTwo(number);
+//   print("The next power of two greater than or equal to $number is $nextPower");
+// }
 
-}
 
-void fibs() {
+int nextPowerOfTwo(int n) {
+  int result = 1;
+  while (result < n) {
+    result += 2;
+  }
 
-}
-
-void numberOfTimes() {
-
-}
-
-void countDown() {
-
+  return result;
 }
 
 void main() {
-
+  int number = 4;
+  int nextPower = nextPowerOfTwo(number);
+  print("The next power of two greater than or equal to $number is $nextPower");
 }
